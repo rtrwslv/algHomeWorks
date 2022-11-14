64bf08851f7d18a826ca3016a44949c37ce59ac9
rtrwslv <yarsl.filatov@gmail.com>
2022-11-14 20:56:11 +0300

    удалил файлы

--- 5.py
+++
@@ -1,9 +0,0 @@
-
-prices = [8, 6, 7, 7]
-result = len(prices)
-for i in range(len(prices)):
-    for j in range(i + 1, len(prices)):
-        if prices[i] - prices[j] == 1:
-            print(prices[i], prices[j])
-            result += 1
-print(result)
