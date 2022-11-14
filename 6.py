a921bf145946b02f2af6827d36f0ff29576679af
rtrwslv <yarsl.filatov@gmail.com>
2022-11-14 20:44:11 +0300

    added homeworks

---
+++ 6.py
@@ -0,0 +1,17 @@
+#O(n)
+
+def rob(nums):
+    def simple_rob(nums):
+        rob, not_rob = 0, 0
+        for num in nums:
+            rob, not_rob = not_rob + num, max(rob, not_rob)
+            print(rob, not_rob)
+        return max(rob, not_rob)
+    if not nums:
+        return 0
+    elif len(nums) == 1:
+        return nums[0]
+    else:
+        return max(simple_rob(nums[1:]), simple_rob(nums[:-1]))
+
+print(rob([1, 2, 3, 1]))
